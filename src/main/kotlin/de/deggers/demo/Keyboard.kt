5e0c98f2c4047bedb7316e3294d5b1a259f16c84
Dustyn Eggers <8664081+deggers@users.noreply.github.com>
2020-11-06 21:02:51 +0100

    WIP show what has been typed

--- src/main/kotlin/de/deggers/demo/Keyboard.kt
+++ src/main/kotlin/de/deggers/demo/Keyboard.kt
@@ -1,41 +1,69 @@
 package de.deggers.demo
 
 import javafx.geometry.Pos
-import javafx.scene.text.TextAlignment
+import javafx.scene.control.Button
+import javafx.scene.paint.Color
 import tornadofx.*
-import javax.swing.GroupLayout
 
 data class Key(val label: String, val size: Double = 18.00)
 
 class Keyboard : View() {
 
-    private val firstRow = listOf(
-        Key(label = "Tab", size = 28.00),
-        Key(label = "Q"),
-        Key(label = "W")
-    )
+  private val tab = button { text = "tab" }.also { it.alignment = Pos.CENTER }
+  private val q = button { text = "q" }.also { it.alignment = Pos.CENTER }
+  private val w = button { text = "w" }.also { it.alignment = Pos.CENTER }
+
+  private val firstRow: List<Button> = listOf(
+    tab,
+    q,
+    w
+  )
 
   override val root = vbox {
     paddingAll = 20
     prefWidth = 800.00
     prefHeight = 300.00
 
+    setOnKeyPressed { event ->
+      if (event.text.toLowerCase() == "q") {
+        q.style {
+          maxWidth = 18.mm
+          minWidth = 18.mm
+          maxHeight = 18.mm
+          minHeight = 18.mm
+          padding = CssBox(-8.mm, 0.mm, 0.mm, -8.mm)
+          fontSize = 22.px
+          backgroundColor += Color.DARKBLUE }
+      }
+    }
+
+    setOnKeyReleased { event ->
+      if (event.text.toLowerCase() == "q") {
+        q.style {
+          maxWidth = 18.mm
+          minWidth = 18.mm
+          maxHeight = 18.mm
+          minHeight = 18.mm
+          padding = CssBox(-8.mm, 0.mm, 0.mm, -8.mm)
+          fontSize = 22.px
+          }
+      }
+    }
+
     hbox {
       spacing = 5.05
       alignment = Pos.TOP_LEFT
 
-      firstRow.forEach {key: Key ->
-        button{
-          text = key.label
-          alignment = Pos.CENTER
-        }.style {
-          maxWidth = key.size.mm
-          minWidth = key.size.mm
+      firstRow.forEach { button: Button ->
+        button.style {
+          maxWidth = 18.mm
+          minWidth = 18.mm
           maxHeight = 18.mm
           minHeight = 18.mm
           padding = CssBox(-8.mm, 0.mm, 0.mm, -8.mm)
           fontSize = 22.px
         }
+        add(button)
       }
     }
   }
